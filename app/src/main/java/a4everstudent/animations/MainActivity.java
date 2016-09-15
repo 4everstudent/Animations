51a6bf898ab2e575530704798c80fde5158fee02
4EverStudent <mail@4everstudent.com>
2016-09-15 15:54:22 +0100

    Add animations with XML

--- app/src/main/java/a4everstudent/animations/MainActivity.java
+++ app/src/main/java/a4everstudent/animations/MainActivity.java
@@ -3,22 +3,34 @@ package a4everstudent.animations;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+//solving the problem of instant animation with help of fellow students of the course
 
-    public void fade(View view){
+    public void challenge(View view){
 
-        ImageView catwoman = (ImageView) findViewById(R.id.CatWoman);
         ImageView deadpool = (ImageView) findViewById(R.id.deadpool);
 
-        //fade out catwoman image
-        catwoman.animate().alpha(0f).setDuration(2000);
-        //fade in deadpool image
-        deadpool.animate().alpha(1f).setDuration(2000);
+        //will call the xml file to get the setted animations config
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anidead);
+        deadpool.startAnimation(animation);
 
     }
+
+      /*
+        //left translation
+        deadpool.animate().translationX(-1000f).setDuration(1000);
+        //right translation
+        deadpool.animate().translationX(1000f).setDuration(2000);
+        //Zoom in full screen
+        deadpool.animate().scaleX(2f).scaleY(2f).setDuration(3000);
+*/
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
